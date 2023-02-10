340b8d87e6084b0f454204f35b78dc37e8a55806
TheoCaron <caron-theo@laposte.net>
2023-02-10 11:00:45 +0100

    ensembling with randomforest

--- src/modelling/model_train_rf.py
+++ src/modelling/model_train_rf.py
@@ -9,16 +9,37 @@ from sklearn import preprocessing
 from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
-
+import numpy as np
 def model_training(X_train,X_val,y_train,y_val,X_test):
+
+    # Number of trees in random forest
+    n_estimators = [int(x) for x in np.linspace(start = 100, stop = 1000, num = 10)]
+    
+    # Number of features to consider at every split
+    max_features = ['auto']
+    
+    # Maximum number of levels in tree
+    max_depth = [int(x) for x in np.linspace(5, 30, num = 5)]
+    max_depth.append(None)
+    
+    # Minimum number of samples required to split a node
+    min_samples_split = [2, 5, 10]
+    
+    # Minimum number of samples required at each leaf node
+    min_samples_leaf = [1, 2, 4, 8]
+    
+    # Method of selecting samples for training each tree
+    bootstrap = [True]
 
     pipe_clf = Pipeline([('scaler', preprocessing.StandardScaler())
                          , ('rf', RandomForestClassifier(random_state=42))])
     param_grid = {
-        'rf__n_estimators': [300,400, 500, 600,700,800,100,200], 
-        'rf__max_features': ['auto'],
-        'rf__max_depth': [25,20,30,10,15,10,5,None],
-        'rf__bootstrap': [True,False]
+        'rf__n_estimators': n_estimators, 
+        'rf__max_features': max_features,
+        'rf__max_depth': max_depth,
+        'rf__bootstrap': bootstrap,
+        'rf__min_samples_split':min_samples_split,
+        'rf__min_samples_leaf':min_samples_leaf
         }
     grid = GridSearchCV(estimator=pipe_clf, param_grid=param_grid,n_jobs=-1,verbose=1)
     grid.fit(X_train,y_train)
